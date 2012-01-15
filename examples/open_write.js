e3e1a96e5ecf48e73f8f85b41634e7dae76cc042
Samori Gorse <samorigorse@gmail.com>
2012-01-15 06:25:23 +0100

    Added read

--- examples/open_write.js
+++ examples/open_write.js
@@ -1,16 +1,9 @@
 var Ftdi = require('../index');
-var ftdi = new Ftdi({
-    'vid': 0x0403,
-    'pid': 0x6001,
-    'index':0,
-    'serial': 'A700ethE'
-});
+var ftdi = new Ftdi();
 ftdi.open();
 ftdi.setBaudrate(19200);
 // Turns all relays on, on the USB RLY08
 // http://www.robot-electronics.co.uk/htm/usb_rly08tech.htm
-ftdi.write(String.fromCharCode(110));
-setTimeout(function() {
-    ftdi.write(String.fromCharCode(100));
-    ftdi.close();
-}, 200);
+ftdi.write(String.fromCharCode(100));
+console.log(ftdi.read());
+ftdi.close();

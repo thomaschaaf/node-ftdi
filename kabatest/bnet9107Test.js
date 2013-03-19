edcb9cf4e84201af4173c87500f54be2a723663d
Adriano Raiano <adriano@raiano.ch>
2013-03-19 12:40:17 +0100

    added bnet9107Test

---
+++ kabatest/bnet9107Test.js
@@ -0,0 +1,27 @@
+var ftdi = require('../index');
+
+ftdi.find(0x18d9, 0x01a0, function(err, devices) { console.log(devices);
+  var device = devices[0];
+
+  device.on('error', function() {
+
+  });
+
+  device.on('data', function(data) {
+    console.log(arguments);
+  });
+
+  device.open({
+    baudrate: 38400,
+    databits: 8,
+    stopbits: 1,
+    parity: 'none'
+  }, function(err) {
+
+    device.write([0x03, 0x30, 0x00, 0x33], function(err) {
+
+    });
+
+  });
+
+});
